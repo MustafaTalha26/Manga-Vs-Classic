8418edb354bda0cd1c09e55b36e49b2c9251fb75
MustafaTalha26 <mustafatalhamert26@gmail.com>
2024-04-17 16:19:45 +0300

    aug

--- model.py
+++ model.py
@@ -1,22 +1,29 @@
 import keras
+from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
 import warnings
 from keras.applications import MobileNetV2
-from keras import regularizers
 warnings.filterwarnings('ignore')
+
+trainAug = keras.models.Sequential([
+	keras.layers.Rescaling(scale=1.0 / 255),
+	keras.layers.RandomFlip("horizontal_and_vertical"),
+	keras.layers.RandomZoom(
+		height_factor=(-0.05, -0.15),
+		width_factor=(-0.05, -0.15)),
+	keras.layers.RandomRotation(0.3)
+])
 
 def MobileNetV2_model(input_shape):
     baseModel = MobileNetV2(include_top=False, input_tensor=keras.layers.Input(shape=input_shape))
     for layer in baseModel.layers[:-4]:
         layer.trainable = False
     model = keras.models.Sequential()
+    model.add(trainAug)
     model.add(baseModel)
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(512, activation='relu',
-                                kernel_regularizer=regularizers.L1(l1=2e-5)))
-    model.add(keras.layers.Dense(128, activation='relu'))
-    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dense(32, activation='relu'))
     model.add(keras.layers.Dense(1, activation='sigmoid'))
     return model
 
@@ -25,15 +32,21 @@ train = keras.utils.image_dataset_from_directory(
     seed=1,
     image_size=(224,224),
     batch_size=32,
-    label_mode='binary'
+    label_mode='binary',
+    shuffle=True,
+    interpolation='nearest',
 )
 valid = keras.utils.image_dataset_from_directory(
     "Comics/validation/",
     seed=1,
     image_size=(224,224),
     batch_size=32,
-    label_mode='binary'
+    label_mode='binary',
+    shuffle=True,
+    interpolation='nearest'
 )
+
+#aug_model = data_augmentation_model((224,224,3))
 
 callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 size = (224, 224)
